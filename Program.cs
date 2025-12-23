7c713de9672ea691c92aa92ad3f7d6f51d8caabf
lisofrenia <chesharisa666@gmail.com>
2025-12-23 12:30:54 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenTelemetry().WithMetrics(meterProviderBuilder =>
+{
+    meterProviderBuilder.AddPrometheusExporter();
 
+    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+                          "Microsoft.AspNetCore.Server.Kestrel");
+
+    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+    meterProviderBuilder.AddView("http.server.request.duration",
+        new ExplicitBucketHistogramConfiguration
+        {
+            Boundaries = new double[]
+            {
+                0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
+            }
+        });
+});
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -26,5 +44,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Calculator}/{action=Index}/{id?}");
-
+app.MapPrometheusScrapingEndpoint();
 app.Run();
